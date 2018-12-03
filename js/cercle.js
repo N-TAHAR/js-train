6b5b08de4c2495013255926300689d24b21caa51
N.TAHAR <najibtaharberrabah@gmail.com>
2018-12-03 09:20:42 +0100

    train

--- js/cercle.js
+++ js/cercle.js
@@ -1,5 +1,6 @@
 var home = document.querySelectorAll('.block');
 var img = document.querySelector('section img');
+var title = document.querySelector('h1');
 var x = 0;
 var count = 0;
 for (let i = 0; i <  home.length; i++){
@@ -12,7 +13,9 @@ for (let i = 0; i <  home.length; i++){
       for (let i = 0; i < home.length; i++) {
         home[i].style.transform = 'rotate(-45deg)';
         home[i].style.marginBottom = '40px';
-        img.style.zIndex = '1';        
+        img.style.zIndex = '1';
+        title.classList.add('is-reveal'); 
+        home[i].style.opacity = '1';  
       }
     }
   });
